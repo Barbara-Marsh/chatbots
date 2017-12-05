5f6e15bb024c52fbbb39a0f0674880d4dad734ac
Barbara Marsh <barbara.marsh@live.vu.edu.au>
2017-12-05 13:41:14 +1100

    Update index.blade.php

--- resources/views/Vline/GoogleAssistantBot/index.blade.php
+++ resources/views/Vline/GoogleAssistantBot/index.blade.php
@@ -12,7 +12,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <!--<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">-->
     <title>V/Line Next Five Departures</title>
 
     <!-- Fonts -->
